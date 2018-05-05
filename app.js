7dbf169aef98a20b0a73b67a3095ab379c9f0fe2
Caroline Forslund <cf222ev@student.lnu.se>
2018-05-05 11:13:16 +0200

    Search bar inserted, TODOs added and tweetFinder() updated
    Search bar inserted in pug template. Todos added in app.js, and tweetFinder function updated

--- app.js
+++ app.js
@@ -7,21 +7,33 @@ const express = require('express');
 // const session = require('express-session');
 // const MongoStore = require('connect-mongo')(session);
 // const User = require('./models/user');
-
-// Twitter
 const Twit = require('twit');
 const config = require('./config/twitter-config');
 const T = new Twit(config);
 
+// TODO: Move this function to the right place
 // Get Hashtag results from Twitter
-// filter the public stream by english tweets containing `#apple`
-var stream = T.stream('statuses/filter', { track: '#apple ', language: 'en' })
+function tweetFinder(hashtag){
+  T.get('search/tweets', { q: hashtag + ' since:2017-01-01', count: 20, lang: 'en' }, function(err, data, response) {
+    if(err){
+      // TODO: Make sure to throw a proper Error
+      console.log("An error occured", err);
+    }
+    else {
+      // TODO: Make sure the right values are being saved
+      console.log("Tweet 1", data.statuses[0].text);
+      console.log("Tweet 2", data.statuses[1].text);
+      console.log("Tweet 3", data.statuses[2].text);
+    }
+  })
+}
 
-stream.on('tweet', function (tweet) {
-  console.log(tweet)
-})
+// TODO: This should be triggered by the users search with the search field value as param
+tweetFinder('#pony');
+
 
 // Get Hashtag results from Instagram
+
 
 const app = express();
 
